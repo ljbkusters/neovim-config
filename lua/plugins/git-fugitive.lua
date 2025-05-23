36b48351cc554d147297a1f1a460542858d37746
Luc Kusters <ljbkusters@gmail.com>
2025-05-23 11:16:43 +0200

    Add more keymaps for vim fugitive

--- lua/plugins/git-fugitive.lua
+++ lua/plugins/git-fugitive.lua
@@ -1,8 +1,16 @@
 -- A git wrapper plugin for vim
 return {
   "tpope/vim-fugitive",
-  opts = {
+  config = function()
     vim.keymap.set("n", "<leader>gc", ":Git commit<CR>")
     vim.keymap.set("n", "<leader>ga", ":Git add %<CR>")
-  }
+    vim.keymap.set("n", "<leader>gd", ":Git diff<CR>")
+    vim.keymap.set("n", "<leader>gD", ":Git diff --staged<CR>")
+    vim.keymap.set("n", "<leader>gs", ":Git status<CR>")
+    vim.keymap.set("n", "<leader>gl", ":Git log --oneline<CR>")
+    vim.keymap.set("n", "<leader>gL", ":Git log<CR>")
+    -- git stash
+    vim.keymap.set("n", "<leader>gSs", ":Git stash<CR>")
+    vim.keymap.set("n", "<leader>gSp", ":Git stash pop<CR>")
+  end
 }
