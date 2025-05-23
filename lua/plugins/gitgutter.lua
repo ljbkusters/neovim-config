f95e65fa982dcbbfaadaadc7055f7ad2a3e495d0
Luc Kusters <ljbkusters@gmail.com>
2025-05-23 10:46:22 +0200

    Add gitgutter plugin
    Add a plugin which adds git changes info.

--- lua/plugins/gitgutter.lua
+++ lua/plugins/gitgutter.lua
@@ -1,4 +1,7 @@
 -- Adds `+`/`-`/`~` marks left of linenumbering in order to display git changes
 return {
   "airblade/vim-gitgutter",
+  opts = {
+    vim.keymap.set("n", "<leader>ht", ":GitGutterLineHighlightsToggle<CR>")
+  }
 }
