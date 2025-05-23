5b739f2a8c66c3518f384643c8b7f0d32a496a7a
Luc Kusters <ljbkusters@gmail.com>
2025-05-23 11:18:43 +0200

    Rename lspconfig.lua to lsp.lua

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,7 @@
+-- This file configures the NeoVim LSP client and LSP servers
+--
+-- The NeoVim LSP client is configured using nvim-lspconfig together with
+-- mason and mason-lspconfig
+return {
+    "neovim/nvim-lspconfig",
+}
