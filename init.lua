9a0bd23d7acfb3d66cf82cb8387958d6a472749d
Luc Kusters <ljbkusters@gmail.com>
2025-05-26 22:12:30 +0200

    Add user commands

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 require("config.settings")	-- vim settings (like tabstop, linenum, etc)
+require("config.commands")	-- user commands
 require("config.remap")		  -- default vim functionality key remaps (not plugin remaps!)
 require("config.lazy")		  -- plugin manager, plugins managed under config/plugins/*.lua
