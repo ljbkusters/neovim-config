e322ff191ca6fc03e9350090f1277c5bd667793d
Luc Kusters <ljbkusters@gmail.com>
2025-05-26 14:36:17 +0200

    Update remap.lua

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -4,8 +4,11 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+-- reload neovim config `source vimrc`
+vim.keymap.set("n", "<leader>sv", ":source $MYVIMRC<CR>")
 
 -- ease of life
 vim.keymap.set("n", "J", "mzJ`z") -- J but cursor stays in place
 vim.keymap.set("n", "n", "nzzzv") -- search n but cursor stays in place
 vim.keymap.set("n", "N", "Nzzzv") -- search N but cursor stays in place
+
