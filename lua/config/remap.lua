e0d435793da97292a65c5466fad9dd149f0848f4
Luc Kusters <ljbkusters@gmail.com>
2025-05-26 15:49:19 +0200

    Update remap.lua
    Add <leader>m mapping to open locally defined mappings

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -7,6 +7,9 @@ vim.g.maplocalleader = "\\"
 -- reload neovim config `source vimrc`
 vim.keymap.set("n", "<leader>sv", ":source $MYVIMRC<CR>")
 
+-- open mappings view
+vim.keymap.set("n", "<leader>m", ":map<CR>")
+
 -- ease of life
 vim.keymap.set("n", "J", "mzJ`z") -- J but cursor stays in place
 vim.keymap.set("n", "n", "nzzzv") -- search n but cursor stays in place
