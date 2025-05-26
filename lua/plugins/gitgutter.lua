1c6c7d885398af49d35e8f9363c938876e06a76b
Luc Kusters <ljbkusters@gmail.com>
2025-05-26 14:42:19 +0200

    Update gitgutter plugin config

--- lua/plugins/gitgutter.lua
+++ lua/plugins/gitgutter.lua
@@ -1,7 +1,7 @@
 -- Adds `+`/`-`/`~` marks left of linenumbering in order to display git changes
 return {
   "airblade/vim-gitgutter",
-  opts = {
+  config = function()
     vim.keymap.set("n", "<leader>ht", ":GitGutterLineHighlightsToggle<CR>")
-  }
+  end
 }
