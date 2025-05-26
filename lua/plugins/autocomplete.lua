426c8341bed2535f17fda1c1bd91e571df3c8e2d
Luc Kusters <ljbkusters@gmail.com>
2025-05-26 13:55:29 +0200

    add autocomplete plugins

---
+++ lua/plugins/autocomplete.lua
@@ -0,0 +1,41 @@
+-- This file configures autocompletion using cmp 
+--
+-- It also defines the sources from which autocompletion can occur.
+return {
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "petertriho/cmp-git",
+    "saadparwaiz1/cmp_luasnip",
+    "L3MON4D3/LuaSnip"
+  },
+  config = function()
+    local cmp = require("cmp")
+
+    cmp.setup({
+      expand = function(args)
+        require('luasnip').lsp_expand(args.body)
+      end,
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+      mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      }),
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' }, -- For luasnip users.
+        { name = 'git' },
+      }, {
+        { name = 'buffer' },
+      })
+    })
+  end,
+}
