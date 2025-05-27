d6553f44219baa37694c6ad0abfd144a854d4909
Luc Kusters <ljbkusters@gmail.com>
2025-05-27 11:10:17 +0200

    Make "WQ" mappings bangable

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -1,4 +1,4 @@
 -- user commands
 vim.api.nvim_create_user_command("W", vim.cmd.w, {})
-vim.api.nvim_create_user_command("WQ", function() vim.cmd("wq") end, {})
-vim.api.nvim_create_user_command("Wq", function() vim.cmd("wq") end, {})
+vim.api.nvim_create_user_command("WQ", function() vim.cmd("wq") end, { bang = true })
+vim.api.nvim_create_user_command("Wq", function() vim.cmd("wq") end, { bang = true })
