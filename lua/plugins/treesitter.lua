d4e3003d8cf14d9c9547fde6e84328552cbfe520
Luc Kusters <ljbkusters@gmail.com>
2025-06-16 15:26:08 +0200

    Add ensure_installed to nvim treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,5 +2,13 @@ return {
   "nvim-treesitter/nvim-treesitter",
   tag = "v0.9.3",
   lazy = false,
-  opts = {},
+  opts = {
+    ensure_installed = {
+      "python",
+      "markdown",
+      "lua",
+      "vim",
+      "vimdoc",
+    },
+  },
 }
