e99240240c9ba81eb3ae06ba3d05db5a71307346
Luc Kusters <ljbkusters@gmail.com>
2025-06-23 06:05:50 +0200

    Add commands to open small terminal from neovim

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -1,4 +1,27 @@
 -- user commands
-vim.api.nvim_create_user_command("W", vim.cmd.w, {})
+
+vim.api.nvim_create_user_command('W', function() vim.cmd('w') end, {})
 vim.api.nvim_create_user_command("WQ", function() vim.cmd("wq") end, { bang = true })
 vim.api.nvim_create_user_command("Wq", function() vim.cmd("wq") end, { bang = true })
+
+
+-- runs every time a terminal is opened
+vim.api.nvim_create_autocmd('TermOpen', {
+  group = vim.api.nvim_create_augroup('custom-term-open', {clear = true}),
+  callback = function()
+    -- disable line numbering
+    vim.opt.number = false
+    vim.opt.relativenumber = false
+    -- source bashrc in terminal
+    local job_id = vim.bo.channel
+    vim.fn.chansend(job_id, {"source ~/.config/bash/.bashrc\r\nclear \r\n"})
+  end,
+})
+
+-- open a small terminal below
+vim.keymap.set("n", "<space>st", function()
+  vim.cmd.vnew() -- create new v split
+  vim.cmd.term() -- open terminal
+  vim.cmd.wincmd("J") -- put at bottom of screen
+  vim.api.nvim_win_set_height(0, 8) -- set hight
+end)
