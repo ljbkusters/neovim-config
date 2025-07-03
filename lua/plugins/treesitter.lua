55cb39770eb093c3cbcf7fc73fc2ec7a649f8961
Luc Kusters <ljbkusters@gmail.com>
2025-07-03 14:44:01 +0200

    Fix treesitter config
    The opts parameter that was used to configure treesitter should have
    been applied to `nvim-treesitter.config`. For this reason, a config
    function is now used which correctly applies the treesitter config like
    ensure installed.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,16 +2,23 @@ return {
   "nvim-treesitter/nvim-treesitter",
   tag = "v0.9.3",
   lazy = false,
-  opts = {
-    ensure_installed = {
-      "python",
-      "markdown",
-      "lua",
-      "vim",
-      "vimdoc",
-    },
-    highlight = {
-      enable = true,
-    },
-  },
+  config = function()
+    require('nvim-treesitter.configs').setup({
+      ensure_installed = {
+        "python",
+        "markdown",
+        "lua",
+        "vim",
+        "vimdoc",
+      },
+      ignore_install = {},
+      modules = {},
+      auto_install = false,
+      sync_install = false,
+      highlight = {
+        enable = true,
+      },
+    })
+    require('nvim-treesitter').setup()
+  end
 }
