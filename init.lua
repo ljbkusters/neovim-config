de0e802ad0a46aa1cc79b9beddd29c14c2de295f
Luc Kusters <ljbkusters@gmail.com>
2026-01-03 11:47:06 +0100

    Import pytest automation config file in main init.lua
    This activates the pytest automatic test file creation.

--- init.lua
+++ init.lua
@@ -2,3 +2,6 @@ require("config.settings")	-- vim settings (like tabstop, linenum, etc)
 require("config.commands")	-- user commands
 require("config.remap")		  -- default vim functionality key remaps (not plugin remaps!)
 require("config.lazy")		  -- plugin manager, plugins managed under config/plugins/*.lua
+
+-- add pytest for current file under tests/ directory
+require("config.pytest_automation")
