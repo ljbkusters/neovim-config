db8e12d2f91fa9fcba6e6a09b0568b155de88d4a
Luc Kusters <ljbkusters@gmail.com>
2026-02-18 11:08:58 +0100

    nfluence plugin config

---
+++ lua/plugins/nfluence.lua
@@ -0,0 +1,9 @@
+return {
+  { dir = "~/projects/nfluence" ,
+    opts = {
+      rootAddress = "https://techops-qpark.atlassian.net/",
+      email = "luc.kusters@q-park.com",
+      apiToken = "...",
+    },
+  },
+}
