7f8580651ec520ee988c09ff357b40cd58c93f40
Luc Kusters <ljbkusters@gmail.com>
2026-04-21 13:22:12 +0200

    Make nvim tree floating

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,12 +1,39 @@
 return {
   "nvim-tree/nvim-tree.lua",
   config = function()
+    local HEIGHT_RATIO = 0.8
+    local WIDTH_RATIO = 0.5
     require("nvim-tree").setup({
       sort = {
         sorter = "case_sensitive",
       },
       view = {
-        width = 30,
+        width = function()
+          return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+        end,
+        -- height = 30,
+        float = {
+          enable = true,
+          open_win_config = function()
+            local screen_w = vim.opt.columns:get()
+            local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+            local window_w = screen_w * WIDTH_RATIO
+            local window_h = screen_h * HEIGHT_RATIO
+            local window_w_int = math.floor(window_w)
+            local window_h_int = math.floor(window_h)
+            local center_x = (screen_w - window_w) / 2
+            local center_y = ((vim.opt.lines:get() - window_h) / 2)
+                             - vim.opt.cmdheight:get()
+            return {
+              border = "rounded",
+              relative = "editor",
+              row = center_y,
+              col = center_x,
+              width = window_w_int,
+              height = window_h_int,
+            }
+          end,
+        }
       },
       filters = {
         dotfiles = false,
@@ -30,44 +57,6 @@ return {
     })
 
 
-    -- SOURCE: https://github.com/nvim-tree/nvim-tree.lua/wiki/Recipes#make-q-and-bd-work-as-if-tree-was-not-visible
-    -- Make :bd and :q behave as usual when tree is visible
-    vim.api.nvim_create_autocmd({ 'BufEnter', 'QuitPre' }, {
-      nested = false,
-      callback = function(e)
-        local tree = require('nvim-tree.api').tree
-
-        -- Nothing to do if tree is not opened
-        if not tree.is_visible() then
-          return
-        end
-
-        -- How many focusable windows do we have? (excluding e.g. incline status window)
-        local winCount = 0
-        for _, winId in ipairs(vim.api.nvim_list_wins()) do
-          if vim.api.nvim_win_get_config(winId).focusable then
-            winCount = winCount + 1
-          end
-        end
-
-        -- We want to quit and only one window besides tree is left
-        if e.event == 'QuitPre' and winCount == 2 then
-          vim.api.nvim_cmd({ cmd = 'qall' }, {})
-        end
-
-        -- :bd was probably issued an only tree window is left
-        -- Behave as if tree was closed (see `:h :bd`)
-        if e.event == 'BufEnter' and winCount == 1 then
-          -- Required to avoid "Vim:E444: Cannot close last window"
-          vim.defer_fn(function()
-            -- close nvim-tree: will go to the last buffer used before closing
-            tree.toggle({ find_file = true, focus = true })
-            -- re-open nivm-tree
-            tree.toggle({ find_file = true, focus = false })
-          end, 10)
-        end
-      end
-    })
     -- /SOURCE
     local api = require('nvim-tree.api')
     api.events.subscribe(api.events.Event.TreeOpen, function()
